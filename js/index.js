ce2ee4394dff4e5cdef78e4f0a3cd37a4fe43176
levis <levismunene01@gmail.com>
2024-04-16 20:42:41 +0300

    privacy policy

--- js/index.js
+++ js/index.js
@@ -5,33 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector('#search-form')
     form.addEventListener('submit', (e) => {
         // prevent default behavior
-        e.preventDefault();
-        const input = document.querySelector('#search');
+        e.preventDefault()
+        const input = document.querySelector('#search')
         
+        // If search input is empty, fetch all games
         if(input.value) {
-            fetchGames(input.value);
+            fetchGames(input.value)
         } else {
-            fetchGames(); // If search input is empty, fetch all games
+            fetchGames(); 
         }
     }); 
 
-    // Dropdown toggle functionality
-    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+    // Dropdown toggle 
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle')
     dropdownToggles.forEach(toggle => {
         toggle.addEventListener('click', () => {
             const dropdownMenu = toggle.nextElementSibling;
-            dropdownMenu.classList.toggle('show');
-        });
-    });
-});
+            dropdownMenu.classList.toggle('show')
+        })
+    })
+})
 
 function fetchGames(searchTerm = '') {
     const options = {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-            // Increase the maximum content length allowed
-            "Content-Length": 25000 // You can adjust this value as needed
         },
     };
 
@@ -39,14 +38,14 @@ function fetchGames(searchTerm = '') {
         .then((response) => response.json())
         .then((games) => {
             // Filter games based on the search term
-            const filteredGames = games.filter((game) => game.title.includes(searchTerm));
+            const filteredGames = games.filter((game) => game.title.includes(searchTerm))
 
             // Clear the game container before displaying filtered games
-            const gameContainer = document.querySelector("#games");
-            gameContainer.innerHTML = '';
+            const gameContainer = document.querySelector("#games")
+            gameContainer.innerHTML = ''
 
             // Display filtered games
-            filteredGames.forEach((game) => pcGame(game));
+            filteredGames.forEach((game) => pcGame(game))
         })
         .catch((err) => console.error(err))
 }
@@ -58,46 +57,45 @@ function pcGame(game) {
     parentDiv.classList.add("card")
 
     // Set background image
-    parentDiv.style.backgroundImage = `url('https://i.pinimg.com/564x/ff/08/92/ff0892562cb3f08528694f36bf54079f.jpg')`;
-    parentDiv.style.backgroundSize = "cover";
-    parentDiv.style.backgroundPosition = "center";
+    parentDiv.style.backgroundImage = `url('https://i.pinimg.com/564x/ff/08/92/ff0892562cb3f08528694f36bf54079f.jpg')`
+    parentDiv.style.backgroundSize = "cover"
+    parentDiv.style.backgroundPosition = "center"
     parentDiv.style.gap = '2px'
 
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
 
      // Game image
-     const gameImage = document.createElement("img");
-     gameImage.classList.add("card-img-top");
-     gameImage.src = game.poster;
-     gameImage.alt = game.title;
-    //  gameImage.style.height = '300px'
+     const gameImage = document.createElement("img")
+     gameImage.classList.add("card-img-top")
+     gameImage.src = game.poster
+     gameImage.alt = game.title
  
      // Append game image to card body
-     cardBody.appendChild(gameImage);
+     cardBody.appendChild(gameImage)
 
     // Game title
-    const title = document.createElement("h5");
-    title.className = "card-title";
-    title.innerText = game.title;
-    title.style.minHeight = "50px";
+    const title = document.createElement("h5")
+    title.className = "card-title"
+    title.innerText = game.title
+    title.style.minHeight = "50px"
 
-    cardBody.appendChild(title);
+    cardBody.appendChild(title)
 
     // Game developers
-    const developers = document.createElement("p");
-    developers.classList.add("card-text");
-    developers.textContent = `Developers: ${game.developers}`;
+    const developers = document.createElement("p")
+    developers.classList.add("card-text")
+    developers.textContent = `Developers: ${game.developers}`
 
     // Game publisher(s)
-    const publishers = document.createElement("p");
-    publishers.classList.add("card-text");
-    publishers.textContent = `Publisher(s): ${game["Publisher(s)"]}`;
+    const publishers = document.createElement("p")
+    publishers.classList.add("card-text")
+    publishers.textContent = `Publisher(s): ${game["Publisher(s)"]}`
 
     // Game category
-    const category = document.createElement("p");
-    category.classList.add("card-text");
-    category.textContent = `Category: ${game.category}`;
+    const category = document.createElement("p")
+    category.classList.add("card-text")
+    category.textContent = `Category: ${game.category}`
 
     // Game description
     const description = document.createElement("p");
@@ -115,28 +113,31 @@ function pcGame(game) {
     gamesSold.textContent = `Games Sold: ${game.games_sold}`;
 
     // Button
-    const link = document.createElement("button");
-    link.classList.add("btn", "btn-primary", "show-price");
-    link.textContent = "PURCHASE GAME";
-    link.addEventListener("click", () => {
-        window.alert(
-            "Please provide your payment information to complete the purchase. HERE => DEBIT CARD ..................                 M-PESA NUMBER.....................                                              VISA CARD ................"
-        );
-
-        // Adjust the styles of other elements
-        [publishers, category, gamesSold, description].forEach((elem) => {
-            elem.style.minHeight = "120px";
+    const purchaseButton = document.createElement("button");
+    purchaseButton.classList.add("btn", "btn-primary", "show-price");
+    purchaseButton.textContent = "PURCHASE GAME";
+    purchaseButton.addEventListener("click", () => {
+        // Direct to payment form
+        window.scrollTo({
+            top: document.getElementById('payment-form').offsetTop,
+            behavior: 'smooth'
         });
     });
 
     // Set text color to white for specified elements
-    [title, link, price, developers, publishers, category, gamesSold, description].forEach((elem) => {
+    [title, purchaseButton, price, developers, publishers, category, gamesSold, description].forEach((elem) => {
         elem.style.color = "white";
     });
 
-    cardBody.append(title, link, price, developers, publishers, category, gamesSold, description);
+    cardBody.append(title, purchaseButton, price, developers, publishers, category, gamesSold, description);
     parentDiv.appendChild(cardBody);
 
     // Append the parent div to the game container
     gameContainer.appendChild(parentDiv);
 }
+
+// An auto hiding until one agree the terms and condition
+document.getElementById("showTerms").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("termsAndConditions").style.display = "block";
+});
